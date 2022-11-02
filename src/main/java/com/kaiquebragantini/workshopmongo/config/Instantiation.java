b11510963e2da3b5ccff60a100f468de18ad707d
KikoMaromba <Kaiquebragasantos@gmail.com>
2022-11-02 12:16:45 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/kaiquebragantini/workshopmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.kaiquebragantini.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.kaiquebragantini.workshopmongo.domain.User;
+import com.kaiquebragantini.workshopmongo.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+}
