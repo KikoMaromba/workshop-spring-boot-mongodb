212e54be2d7a093ccf5e59e5e8f6c0723acdb811
KikoMaromba <Kaiquebragasantos@gmail.com>
2022-11-09 11:29:37 -0300

    Consulta simples com query methods

--- src/main/java/com/kaiquebragantini/workshopmongo/services/PostService.java
+++ src/main/java/com/kaiquebragantini/workshopmongo/services/PostService.java
@@ -15,29 +15,13 @@ public class PostService {
 	
 	@Autowired
 	private PostRepository repository;
-	
-	public List<Post> findAll() {
-		return repository.findAll();
-	}
 
 	public Post findById(String id) {
 		Optional<Post> post = repository.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
 	}
 	
-	public void delete(String id) {
-		findById(id);
-		repository.deleteById(id);
-	}
-
-	public Post update(Post obj) {
-		Post post = findById(obj.getId());
-		updateData(post, obj);
-		return repository.save(post);
-	}
-
-	private void updateData(Post post, Post obj) {
-		post.setTitle(obj.getTitle());
-		post.setBody(obj.getBody());
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
 	}
 }
